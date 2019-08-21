cd7c6fda14c4ba0f890d9b8a18d1e4328aa30d15
Patricio <pgmonzon@gmail.com>
2019-08-21 17:19:56 -0300

    chore: minor changes

--- .all-contributors-html.js
+++ .all-contributors-html.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const SOURCE = '.all-contributorsrc';
 const TARGET =
-  './projects/angular-ngrx-material-starter/src/app/features/about/about/about.component.html';
+  './projects/yng_front/src/app/features/about/about/about.component.html';
 const TOKEN_START = '<!-- ALL-CONTRIBUTORS-LIST:START -->';
 const TOKEN_END = '<!-- ALL-CONTRIBUTORS-LIST:END -->';
 const PATTERN = new RegExp(`${TOKEN_START}[\\s\\S]*${TOKEN_END}`, 'gim');
